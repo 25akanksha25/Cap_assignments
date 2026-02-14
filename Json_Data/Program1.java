92708f0e5e2b288bed2fad60a3da9143bfca5e5f
Akanksha Chopra <chopraakanksha10@gmail.com>
2026-02-14 23:49:23 +0530

    json

---
+++ Json_Data/Program1.java
@@ -0,0 +1,20 @@
+package Json_Data;
+
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+public class Program1 {
+    public static void main(String[] args) {
+        JSONArray subjects = new JSONArray();
+        subjects.put("Java");
+        subjects.put("AWS");
+        subjects.put("SQL");
+        JSONObject student = new JSONObject();
+        student.put("name", "Samarpit");
+        student.put("age", 21);
+        student.put("subjects", subjects);
+        System.out.println(student.toString());
+    }
+}
+
