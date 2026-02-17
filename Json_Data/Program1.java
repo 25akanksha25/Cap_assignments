dd7f526b5e5d0f61e08df18435d4226c318f4536
25akanksha25 <123538069+25akanksha25@users.noreply.github.com>
2026-02-17 18:48:48 +0530

    Update

--- Json_Data/Program1.java
+++ Json_Data/Program1.java
@@ -8,10 +8,10 @@ public class Program1 {
     public static void main(String[] args) {
         JSONArray subjects = new JSONArray();
         subjects.put("Java");
-        subjects.put("AWS");
+        subjects.put("Python");
         subjects.put("SQL");
         JSONObject student = new JSONObject();
-        student.put("name", "Samarpit");
+        student.put("name", "Akanksha");
         student.put("age", 21);
         student.put("subjects", subjects);
         System.out.println(student.toString());
